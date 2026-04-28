22b01cd3167cfbe86bf000c6d88a6437b552d31d
pnolann <nolann.pierreantoine@ynov.com>
2026-04-28 21:05:36 +0200

    feat(launcher): clear file dusk

--- src/main/java/artemis/model/launcher/Launcher.java
+++ src/main/java/artemis/model/launcher/Launcher.java
@@ -10,7 +10,7 @@ public abstract class Launcher {
     private double price;
 
     // load the constructor
-    public Launcher(String name, boolean manned, int maxPerson, double maxFuel, int maxBooster, double payLoad, double price){
+    public Launcher(String name, boolean manned, double maxFuel, int maxBooster, double payLoad, double price){
 
         this.name = name;
         this.manned = manned;
