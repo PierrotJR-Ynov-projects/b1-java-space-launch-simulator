0e99abf82d53894dad870912fbcdcfeb094750fc
pnolann <nolann.pierreantoine@ynov.com>
2026-04-28 20:49:01 +0200

    feat(ariane5): ariane5 created with correct information

--- src/main/java/artemis/model/launcher/Ariane5.java
+++ src/main/java/artemis/model/launcher/Ariane5.java
@@ -1,4 +1,14 @@
 package artemis.model.launcher;
 
-public class Ariane5 {
+public class Ariane5 extends Launcher {
+
+    // constructor of ariane5
+    public Ariane5() {
+        super("Ariane5",false,0,700,2,20,180);
+    }
+
+    @Override
+    public void showInformation(){
+        System.out.println("Lanceur : " + this.getName() + " selectionné");
+    }
 }
