0ca7a05faa8b619de0925b9db6a057f46c4d0f34
pnolann <nolann.pierreantoine@ynov.com>
2026-04-28 22:05:42 +0200

    feat(SLS): class created

--- src/main/java/artemis/model/launcher/SLS.java
+++ src/main/java/artemis/model/launcher/SLS.java
@@ -1,4 +1,13 @@
 package artemis.model.launcher;
 
-public class SLS {
+public class SLS extends Launcher {
+
+    public SLS(){
+        super("SLS",true,2600,2,130,2000);
+    }
+
+    @Override
+    public void showInformation(){
+        System.out.println("Lanceur sélectionné: "+getName());
+    }
 }
