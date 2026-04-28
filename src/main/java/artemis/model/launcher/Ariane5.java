b6bc43c0eb12b766a370ed632ddf2ee4ce3e8eff
pnolann <nolann.pierreantoine@ynov.com>
2026-04-28 21:05:01 +0200

    feat(ariane5): void eddited and maxPerson delete

---
+++ src/main/java/artemis/model/launcher/Ariane5.java
@@ -0,0 +1,13 @@
+package artemis.model.launcher;
+
+public class Ariane5 extends Launcher {
+
+    public Ariane5(){
+        super("Ariane 5",false,700,2,20,180);
+    }
+
+    @Override
+    public void showInformation(){
+        System.out.println("Lanceur selectioné : " + getName());
+    }
+}
