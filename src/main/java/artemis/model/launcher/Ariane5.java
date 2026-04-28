2f60fa3aedffb649147675664d01a2fb05a7b0c4
pnolann <nolann.pierreantoine@ynov.com>
2026-04-28 20:51:18 +0200

    feat(ariane5) change launcher's name

--- src/main/java/artemis/model/launcher/Ariane5.java
+++ src/main/java/artemis/model/launcher/Ariane5.java
@@ -4,7 +4,7 @@ public class Ariane5 extends Launcher {
 
     // constructor of ariane5
     public Ariane5() {
-        super("Ariane5",false,0,700,2,20,180);
+        super("Ariane 5",false,0,700,2,20,180);
     }
 
     @Override
