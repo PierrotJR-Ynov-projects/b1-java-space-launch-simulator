4a07d77c72b3c18f131a4cbbe7227130c5e48b1b
pnolann <nolann.pierreantoine@ynov.com>
2026-04-29 14:11:55 +0200

    feat(Booster): add comments

--- src/main/java/artemis/model/booster/Booster.java
+++ src/main/java/artemis/model/booster/Booster.java
@@ -3,9 +3,9 @@ package artemis.model.booster;
 public abstract class Booster {
 
     private String name;
-    private double extraBoost;
-    private double mass;
-    private double price;
+    private double extraBoost; // in kN
+    private double mass; // in tons
+    private double price; // in millions of €
 
     public Booster(String name, double extraBoost, double mass, double price){
         this.name = name;
