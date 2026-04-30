0322e28f1891bbdaaa184cdd9dbb4a31172c9f25
pnolann <nolann.pierreantoine@ynov.com>
2026-04-30 15:38:08 +0200

    feat(lune): create class lune

---
+++ src/main/java/artemis/model/mission/Lune.java
@@ -0,0 +1,17 @@
+package artemis.model.mission;
+
+public class Lune extends Mission{
+
+    public Lune(){
+        super("Lune",true,400000,10,8,0.005);
+    }
+
+    @Override
+    public String showDescription(){
+        return "Mission : Réaliser un aller-retour sur la Lune dans le but d'obtenir de la roche lunaire.";
+    }
+    public void missionCompleted(){
+        return false;
+    }
+
+}
