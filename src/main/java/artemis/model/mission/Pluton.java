2f9079758c19954008f7419e4d886ab9ff42b152
pnolann <nolann.pierreantoine@ynov.com>
2026-04-30 16:08:53 +0200

    feat(pluton): custom mission created

--- src/main/java/artemis/model/mission/Pluton.java
+++ src/main/java/artemis/model/mission/Pluton.java
@@ -1,4 +1,11 @@
 package artemis.model.mission;
 
-public class Pluton {
+public class Pluton extends Mission{
+    public Pluton(){
+        super("Pluton",false,5000000000L,"9 à 10 ans",0.000001);
+    }
+    @Override
+    public String showDescription(){
+        return "Mission : Les USA souhaitent lancer une fusée sur Pluton pour prouver qu'il s'agit d'une planète";
+    }
 }
