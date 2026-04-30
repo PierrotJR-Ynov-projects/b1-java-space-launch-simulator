5b085501ac739de2cedcc52fbea984d88f32295b
pnolann <nolann.pierreantoine@ynov.com>
2026-04-30 16:00:58 +0200

    feat(orbite): class created

--- src/main/java/artemis/model/mission/Orbite.java
+++ src/main/java/artemis/model/mission/Orbite.java
@@ -1,4 +1,13 @@
 package artemis.model.mission;
 
-public class Orbite {
+public class Orbite extends Mission{
+
+    public Orbite(){
+        super("Orbite terrestre",false,400,"Quelques heures",1);
+    }
+
+    @Override
+    public String showDescription() {
+        return "Mission : Envoie d'une fusée hors de l'orbite terrestre.";
+    }
 }
