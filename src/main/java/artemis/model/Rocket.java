10fd1d2d6b45803df30cc1146c779a277cd37f25
pnolann <nolann.pierreantoine@ynov.com>
2026-04-30 16:54:12 +0200

    feat(rocket): add class rocket

---
+++ src/main/java/artemis/model/Rocket.java
@@ -0,0 +1,36 @@
+package artemis.model;
+
+import artemis.model.booster.Booster;
+import artemis.model.launcher.Launcher;
+import artemis.model.capsule.Capsule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rocket {
+    private String name;
+    private Launcher launcher;
+    private Capsule capsule;
+    private List<Booster> booster;
+
+    public Rocket(String name, Launcher launcher, Capsule capsule) {
+        this.name = name;
+        this.launcher = launcher;
+        this.capsule = capsule;
+        this.booster = new ArrayList<>();
+    }
+
+    public void addBooster(Booster newBooster){
+        if ( this.booster.size() > this.launcher.getMaxBooster() ){
+            this.booster.add(newBooster);
+        } else {
+            System.out.println("Erreur : le lanceur" + this.launcher.getName() + " a atteint sa limite de " + this.launcher.getMaxBooster() + " boosters. Total de booster(s) : " + this.booster.size());
+        }
+    }
+
+    // getters
+    public String getName() { return name; }
+    public Launcher getLauncher() { return launcher; }
+    public Capsule getCapsule() { return capsule; }
+    public List<Booster> getBoosters() { return booster; }
+}
